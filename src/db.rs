58c672c3efca940ac66dc2d95617063155050faa
wzy <2049562063@qq.com>
2024-10-03 15:40:41 +0800

    add conn store

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,42 @@
 use crate::account::{Account, Username};
+use crate::device::Device;
 use crate::event::Task;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::sync::RwLock;
-use crate::device::Device;
+use tokio::sync::mpsc;
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Debug, Default)]
 pub struct DB {
     pub users: RwLock<HashMap<Username, Account>>,
+    pub devices: RwLock<HashMap<Username, Vec<Device>>>,
+    tasks: Tasks,
+    pub conn: RwLock<HashMap<Username, Vec<mpsc::Sender<SSEMessage>>>>,
+}
+
+impl DB {
+    pub(crate) fn event(&self) -> &RwLock<HashMap<Username, Vec<Task>>> {
+        &self.tasks.event
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct SSEMessage {
+    message: String,
+}
+
+impl SSEMessage {
+    pub fn new(message: &str) -> Self {
+        Self {
+            message: message.to_string(),
+        }
+    }
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Tasks {
     pub event: RwLock<HashMap<Username, Vec<Task>>>,
-    pub devices: RwLock<HashMap<Username, Vec<Device>>>
 }
