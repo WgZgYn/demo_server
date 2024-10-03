a65a9dbec278c9725205632fd027eda09701082c
wzy <2049562063@qq.com>
2024-10-03 15:41:36 +0800

    add sse support

--- src/event.rs
+++ src/event.rs
@@ -1,9 +1,9 @@
 use crate::account::Account;
-use crate::db::DB;
+use crate::db::{SSEMessage, DB};
+use crate::device::DeviceId;
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use crate::device::DeviceId;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Task {
@@ -24,16 +24,13 @@ pub struct GetTask {
 
 #[derive(Serialize, Deserialize)]
 pub struct PostTask {
-    account: Account,
-    task: Task,
+    pub account: Account,
+    pub task: Task,
 }
 
 pub async fn get_task(data: web::Data<DB>, msg: web::Json<GetTask>) -> HttpResponse {
     let id = &msg.account.username;
-    let mut tasks = data
-        .event
-        .write()
-        .expect("event not read");
+    let mut tasks = data.event().write().expect("event not read");
 
     if let Some(tasks) = tasks.get_mut(id) {
         let v: Vec<Task> = tasks.drain(..).collect();
@@ -50,14 +47,27 @@ pub async fn get_task(data: web::Data<DB>, msg: web::Json<GetTask>) -> HttpRespo
 }
 
 pub async fn post_task(data: web::Data<DB>, msg: web::Json<PostTask>) -> HttpResponse {
+    println!("POST task");
+
     let id = msg.account.username.clone();
     let task = msg.into_inner().task;
-    data.event
+
+    data.event()
         .write()
         .expect("Failed to write event")
-        .entry(id)
+        .entry(id.clone())
         .or_default()
         .push(task);
+
+    let mut ss = data.conn.write().expect("Failed to write connection");
+
+    let ss = ss.entry(id).or_default();
+
+    // TODO: remove the disconnect sender
+    for s in ss {
+        println!("send update to sse");
+        s.send(SSEMessage::new("update")).await.expect("can't send");
+    }
 
     HttpResponse::Ok().json(json!(
         {
