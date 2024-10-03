68351c7501b599c1858d4c2969c8c2289060e17a
wzy <2049562063@qq.com>
2024-10-03 15:41:53 +0800

    add mod

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 mod account;
 mod db;
-mod event;
 mod device;
+mod event;
+mod sse;
 mod test;
 
+use crate::account::{get_account, post_account};
 use crate::db::DB;
+use crate::device::get_device;
+use crate::event::{get_task, post_task};
+use crate::sse::sse_handler;
+use crate::test::{ping, test_task};
+use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::borrow::Cow;
 use std::sync::Mutex;
-use crate::account::{get_account, post_account};
-use crate::device::get_device;
-use crate::event::{get_task, post_task};
-use crate::test::{ping, test_task};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Message {
@@ -43,22 +46,19 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let counter = counter.clone();
         App::new()
+            .wrap(Cors::permissive())
             .app_data(counter.clone())
             .app_data(db.clone())
-
             .route("/ping", web::get().to(ping))
             .route("/task", web::get().to(get_task))
             .route("/task", web::post().to(post_task))
-
             .route("/account", web::post().to(post_account))
             .route("/account", web::get().to(get_account))
-
             .route("/device", web::get().to(get_device))
-
             .route("/device/{id}/{ops}", web::get().to(test_task))
-
             .route("/message", web::get().to(get_message))
             .route("/message", web::post().to(post_message))
+            .route("/sse", web::get().to(sse_handler))
     })
     .bind("0.0.0.0:8080")?
     .run()
