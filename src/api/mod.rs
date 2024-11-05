37c2f3ca7758728e1e76fa59b4a1af660afb7e0a
wzy <2049562063@qq.com>
2024-11-05 11:25:23 +0800

    split api

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,19 +2,15 @@ use crate::api::auth::{validator, Auth};
 use crate::api::login::{login, login_token};
 use crate::api::my::config_my;
 use crate::api::signup::signup;
-use crate::api::test::{get_auth_info, ping, test_get_account, test_task};
-use crate::db::device::get_device;
-use crate::db::event::{get_task, post_task};
+use crate::api::test::{config_test};
 use actix_web::web;
 use actix_web_httpauth::middleware::HttpAuthentication;
-use crate::api::sse::{sse_handler, sse_test};
 
 pub mod auth;
 pub mod login;
 pub mod my;
 pub mod signup;
 pub mod test;
-pub mod sse;
 
 pub fn config_api(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -29,35 +25,15 @@ pub fn config_api(cfg: &mut web::ServiceConfig) {
             .configure(config_my)
             .service(
                 web::scope("/sse")
-                    .route("", web::get().to(sse_handler))
-                    .route("/test", web::get().to(sse_test)),
-            )
-            .service(
-                web::resource("/account")
-                    .wrap(Auth)
-                    .route(web::get().to(test_get_account)),
             )
             .service(
                 web::resource("/task")
                     .wrap(Auth)
-                    .route(web::get().to(get_task))
-                    .route(web::post().to(post_task)),
             )
             .service(
                 web::scope("/device")
                     .wrap(HttpAuthentication::bearer(validator))
-                    .route("", web::get().to(get_device))
-                    .route("/{id}/{ops}", web::get().to(test_task)),
             )
-            .service(
-                web::scope("/test")
-                    .service(web::resource("/ping").route(web::get().to(ping)))
-                    .service(
-                        web::scope("/auth")
-                            .wrap(Auth)
-                            .route("", web::get().to(get_auth_info))
-                            .route("/task/{id}/{ops}", web::post().to(test_task)),
-                    ),
-            )
+            .configure(config_test),
     );
 }
--- src/api/my/mod.rs
+++ src/api/my/mod.rs
@@ -1,24 +1,50 @@
 use crate::api::auth::Auth;
-use crate::api::my::area::add_area_api;
-use crate::api::my::device::{add_device_api, show_devices_api};
-use crate::api::my::house::add_house_api;
+use crate::api::my::area::*;
+use crate::api::my::device::*;
+use crate::api::my::house::*;
+use crate::api::my::user_info::*;
 use actix_web::web;
 use actix_web::web::ServiceConfig;
 
-pub mod area;
-pub mod device;
-pub mod house;
+mod area;
+mod device;
+mod house;
+mod user_info;
 
 pub fn config_my(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("/my")
             .wrap(Auth)
-            .service(web::resource("/area").route(web::post().to(add_area_api)))
             .service(
-                web::resource("/device")
-                    .route(web::get().to(show_devices_api))
-                    .route(web::post().to(add_device_api)),
+                web::resource("/area")
+                    .route(web::post().to(add_area_api))
+                    .route(web::get().to(query_area_api))
+                    .route(web::delete().to(delete_area_api))
+                    .route(web::patch().to(update_area_api)),
             )
-            .service(web::resource("/house").route(web::post().to(add_house_api)))
+            .service(
+                web::scope("/device")
+                    .service(
+                        web::resource("")
+                            .route(web::get().to(query_devices_api))
+                            .route(web::post().to(add_device_api))
+                            .route(web::delete().to(delete_device_api))
+                            .route(web::patch().to(update_device_api)),
+                    )
+                    .route("/{id}/{service}", web::get().to(device_service_api))
+                    .route("/{id}", web::get().to(device_status_api)),
+            )
+            .service(
+                web::resource("/house")
+                    .route(web::post().to(add_house_api))
+                    .route(web::get().to(query_house_api))
+                    .route(web::delete().to(delete_house_api))
+                    .route(web::patch().to(update_house_api)),
+            )
+            .service(
+                web::resource("/info")
+                    .route(web::post().to(add_user_info_api))
+                    .route(web::get().to(query_user_info_api)),
+            ),
     );
 }
