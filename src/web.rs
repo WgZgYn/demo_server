9b0d6ee95131474c838997b3c1aa00f630d15c23
wzy <2049562063@qq.com>
2024-11-05 11:26:32 +0800

    nothing

--- src/web.rs
+++ src/web.rs
@@ -1,23 +1,19 @@
 use actix_files::Files;
-use actix_web::{web, HttpResponse};
 use actix_web::web::ServiceConfig;
+use actix_web::{web, HttpResponse};
 use log::info;
 
 pub fn vue() -> Files {
     Files::new("/", "./static").index_file("index.html")
 }
-
 pub fn config_web(cfg: &mut ServiceConfig) {
     cfg.service(vue());
 }
-
 pub fn config_redirects(cfg: &mut ServiceConfig) {
-    cfg.default_service(
-        web::get().to(|| {
-            info!("redirect to /");
-            let mut b = HttpResponse::PermanentRedirect();
-            b.append_header(("Location", "/"));
-            b
-        })
-    );
+    cfg.default_service(web::get().to(|| {
+        info!("redirect to /");
+        let mut b = HttpResponse::PermanentRedirect();
+        b.append_header(("Location", "/"));
+        b
+    }));
 }
