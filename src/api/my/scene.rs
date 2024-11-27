aae412b7f5c316c31546d3b86d494cc598403d32
wzy <2049562063@qq.com>
2024-11-27 13:05:35 +0800

    add scene

--- src/api/my/scene.rs
+++ src/api/my/scene.rs
@@ -1,8 +1,9 @@
 use crate::db::DataBase;
 use crate::dto::http::request::SceneAdd;
 use crate::utils;
-use actix_web::{web, HttpRequest, HttpResponse};
+use actix_web::{web, HttpMessage, HttpRequest, HttpResponse};
 use log::error;
+use crate::security::auth::Claims;
 
 pub async fn add_scene(
     data: web::Json<SceneAdd>,
@@ -27,7 +28,40 @@ pub async fn add_scene(
     }
 }
 
-pub async fn delete_scene() -> HttpResponse {
-    // TODO:
-    HttpResponse::NotFound().finish()
+pub async fn delete_scene(id: web::Path<i32>, db: web::Data<DataBase>) -> HttpResponse {
+    let mut session = match db.get_session().await {
+        Ok(session) => session,
+        Err(e) => {
+            error!("{e}");
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+    match session.delete_scene(id.into_inner()).await {
+        Ok(_) => HttpResponse::Ok().json(utils::Result::success()),
+        Err(e) => {
+            error!("{e}");
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+pub async fn get_scene(req: HttpRequest, db: web::Data<DataBase>) -> HttpResponse {
+    let e = req.extensions();
+    let claims = match e.get::<Claims>() {
+        Some(claims) => claims,
+        None => return HttpResponse::Unauthorized().finish(),
+    };
+
+    let session = match db.get_session().await {
+        Ok(session) => session,
+        Err(e) => {
+            error!("{}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    match session.get_scene(claims.id()).await {
+        Ok(_) => HttpResponse::Ok().json(utils::Result::success()),
+        Err(e) => { error!("{}", e); HttpResponse::InternalServerError().finish() }
+    }
 }
--- src/db/data/scene.rs
+++ src/db/data/scene.rs
@@ -3,11 +3,16 @@ use crate::dto::http::request::SceneAdd;
 use tokio_postgres::Error;
 
 impl Session {
+    // TODO:
     pub async fn add_scene(&mut self, data: SceneAdd) -> Result<(), Error> {
         Ok(())
     }
 
     pub async fn delete_scene(&mut self, scene_id: i32) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub async fn get_scene(&self, account_id: i32) -> Result<(), Error> {
         Ok(())
     }
 }
