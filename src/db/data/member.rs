639b0d413a673e58dadd412a63f680bd06754b3a
wzy <2049562063@qq.com>
2024-12-05 15:40:26 +0800

    fix

--- src/db/data/member.rs
+++ src/db/data/member.rs
@@ -20,10 +20,11 @@ impl Session {
 
     pub async fn get_member(&self, account_id: i32) -> Result<MemberInfo, Error> {
         let rows = self.0.query("\
-        SELECT (h.house_id, h.house_name, a.account_id, a.username) \
+        SELECT h.house_id, h.house_name, a.account_id, a.username \
         FROM (SELECT house_id FROM member m WHERE m.account_id = $1) \
+        JOIN member USING(house_id) \
         JOIN house h USING(house_id) \
-        JOIN account a USING(account_id)", &[&account_id]).await?;
+        JOIN account a USING(account_id) ", &[&account_id]).await?;
         let mut houses = Vec::<HouseMember>::new();
         for row in &rows {
             let account_id: i32 = row.get("account_id");
@@ -50,5 +51,52 @@ impl Session {
             }
         }
         Ok(MemberInfo { houses_member: houses })
+    }
+
+    pub async fn get_member_by_house_id(&self, house_id: i32) -> Result<MemberInfo, Error> {
+        let rows = self.0.query("\
+        SELECT h.house_id, h.house_name, a.account_id, a.username \
+        FROM (SELECT house_id FROM member m WHERE m.house_id = $1) \
+        JOIN member USING(house_id) \
+        JOIN house h USING(house_id) \
+        JOIN account a USING(account_id) ", &[&house_id]).await?;
+        let mut houses = Vec::<HouseMember>::new();
+        for row in &rows {
+            let account_id: i32 = row.get("account_id");
+            let username: String = row.get("username");
+            let house_id: i32 = row.get("house_id");
+            let house_name: String = row.get("house_name");
+
+            match houses.iter_mut().find(|h| h.house_info.house_id == house_id) {
+                Some(v) => {
+                    v.account.push(AccountInfo { account_id, username })
+                }
+                None => {
+                    houses.push(HouseMember {
+                        house_info: HouseInfo {
+                            house_id,
+                            house_name,
+                        },
+                        account: vec![AccountInfo {
+                            account_id,
+                            username
+                        }],
+                    })
+                }
+            }
+        }
+        Ok(MemberInfo { houses_member: houses })
+    }
+
+    pub async fn get_account_ids_by_house_id(&self, house_id: i32) -> Result<Vec<i32>, Error> {
+        self.0.query("SELECT account_id FROM member WHERE house_id = $1", &[&house_id])
+            .await
+            .map(|rows| rows.into_iter().map(|row| row.get("account_id")).collect())
+    }
+
+    pub async fn get_house_ids_by_account_id(&self, account_id: i32) -> Result<Vec<i32>, Error> {
+        self.0.query("SELECT house_id FROM member WHERE account_id = $1", &[&account_id])
+            .await
+            .map(|rows| rows.into_iter().map(|row| row.get("account_id")).collect())
     }
 }
