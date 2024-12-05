a772433b0b3ea85b000ba8d135870243cc60c14c
wzy <2049562063@qq.com>
2024-12-05 11:29:59 +0800

    sse test

--- src/api/test/mod.rs
+++ src/api/test/mod.rs
@@ -8,6 +8,7 @@ use actix_web::{web, HttpResponse};
 pub use ping::ping;
 use rumqttc::AsyncClient;
 pub use test_auth::get_auth_info;
+use crate::api::sse::sse_test;
 
 async fn test_mqtt(client: web::Data<AsyncClient>) -> HttpResponse {
     println!("test_mqtt client: {:?}", client);
@@ -19,6 +20,7 @@ pub fn config_test(cfg: &mut ServiceConfig) {
         web::scope("/test")
             .service(web::resource("/ping").route(web::get().to(ping)))
             .service(web::resource("/mqtt").route(web::get().to(test_mqtt)))
+            .service(web::resource("/sse").route(web::get().to(sse_test)))
             .service(
                 web::scope("/auth")
                     .wrap(Auth)
