a786c5503730597a85caa26ab2b4922f70ea4015
wzy <2049562063@qq.com>
2024-12-05 15:39:55 +0800

    house delete

--- src/db/data/house.rs
+++ src/db/data/house.rs
@@ -1,16 +1,12 @@
 use crate::db::database::Session;
 use crate::dto::entity::simple::HouseInfo;
 use crate::dto::http::request::HouseUpdate;
-use deadpool_postgres::PoolError;
+use deadpool_postgres::{GenericClient, PoolError};
 use tokio_postgres::Error;
 
 impl Session {
-    // TODO:
-    pub async fn update_house_info(
-        &self,
-        house_id: i32,
-        data: HouseUpdate,
-    ) -> Result<(), PoolError> {
+    pub async fn update_house_info(&self, house_id: i32, data: HouseUpdate) -> Result<(), PoolError> {
+        self.0.execute("UPDATE house SET house_name = $1 WHERE house_id = $2", &[&data.house_name, &house_id]).await?;
         Ok(())
     }
 
@@ -28,12 +24,16 @@ impl Session {
         })
     }
 
+    pub async fn is_house_created_by(&self, house_id: i32, account_id: i32) -> Result<bool, PoolError> {
+        let creator = self.0.query_one("SELECT created_by FROM house WHERE house_id = $1", &[&house_id]).await?;
+        let created_by: i32 = creator.get("created_by");
+        Ok(created_by == account_id)
+    }
     pub async fn delete_house(&self, house_id: i32) -> Result<u64, Error> {
         self.0
             .execute("DELETE FROM house WHERE house_id = $1", &[&house_id])
             .await
     }
-
     pub async fn add_house(&mut self, house_name: &str, account_id: i32) -> Result<(), Error> {
         let trans = self.0.transaction().await?;
         let row = trans
